1c2507e92483d3134784d321836cbb8538247d4c
Arif Rahman <arifindo.rahman@gmail.com>
2020-08-15 08:03:43 +0700

    change 70 to 1000 data in code

--- data_coll.py
+++ data_coll.py
@@ -3,6 +3,6 @@ import pandas as pd
 
 path = "C:/Users/arifi/Documents/da_salary_proj/chromedriver"
 
-df = gs.get_jobs('data analyst',70, False, path, 15)
+df = gs.get_jobs('data analyst',1000, False, path, 15)
 
 df.to_csv('glassdoor_jobs.csv', index = False)
